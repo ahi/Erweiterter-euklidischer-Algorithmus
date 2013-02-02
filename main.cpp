d32d44eaaa9aec8858b57ba6c70401c0f52eee97
Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2013-02-02 17:30:36 +0100

    Create C Project with Netbeans

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ahmet
+ *
+ * Created on 2. Februar 2013, 17:25
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
